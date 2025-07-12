c968bb24589ce843bfac422bcaf881f80c6d610d
Nkateko Maluleke <nkateko.maluleke03@gmail.com>
2025-07-12 20:14:41 +0200

    .Net REST API part removed. Only Angular Ionic project left in the repo.

--- PlanUP_CSharp_API/PlanUP/Models/LoginVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PlanUP.Models
-{
-    public class LoginVM
-    {
-        public string EmailAddress { get; set; }
-        public string Password { get; set; }
-    }
-}
--- PlanUP_CSharp_API/PlanUP/Repositories/IEmailRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PlanUP.Repositories
-{
-    public interface IEmailRepository
-    {
-        Task ExecuteRegisterEmail(string receiverEmail, string receiptientNames, string temporalPassword, string userID);
-        Task ExecuteResetPasswordEmail(string receiverEmail, string receiptientNames, string token, string userID);
-    }
-}
